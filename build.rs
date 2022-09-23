a2168f58474ebf600c90eec3df672f52509daa8e
Jiawei Zhao <Phoenxi500526@163.com>
2022-09-23 17:45:28 +0800

    add protocol definition in protobuf

---
+++ build.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut config = prost_build::Config::new();
+    // 为 protobuf 的 bytes 类型生成 Bytes 而非缺省的 Vec<u8>。
+    // . 表示通配符，即为所有的 bytes 都成成 bytes::Bytes。这样效率更高，解析的时候就需要使用 bytes 了
+    config.bytes(&["."]);
+    // 为 protobuf 生成的协议添加偏序 Trait
+    config.type_attribute(".", "#[derive(PartialOrd)]");
+    config
+        .out_dir("src/pb")
+        .compile_protos(&["abi.proto"], &["."])
+        .unwrap();
+}

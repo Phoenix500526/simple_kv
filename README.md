6c33d6124d1c34b706a5cd750e680b949004b683
Jiawei Zhao <Phoenxi500526@163.com>
2022-10-23 16:28:59 +0800

    adding pattern subscribe mode

--- README.md
+++ README.md
@@ -22,7 +22,7 @@
 
 
 ## Todo
-- [ ] Redis 还支持 PSUBSCRIBE，也就是说除了可以 subscribe “chat” 这样固定的 topic，还可以是 “chat.*”，一并订阅所有 “chat”、“chat.rust”、“chat.elixir” 。想想看，如果要支持 PSUBSCRIBE，你该怎么设计 Broadcaster 里的两张表？
+- [x] Redis 还支持 PSUBSCRIBE，也就是说除了可以 subscribe “chat” 这样固定的 topic，还可以是 “chat.*”，一并订阅所有 “chat”、“chat.rust”、“chat.elixir” 。想想看，如果要支持 PSUBSCRIBE，你该怎么设计 Broadcaster 里的两张表？
 - [ ] 除了 TLS，另外一个被广泛使用的处理应用层安全的协议是 noise protocol。Rust 下有 snow 这个很优秀的库处理 noise protocol。对于有余力的同学，你们可以看看它的文档，尝试着写段类似 tls.rs 的代码，让我们的 kvs / kvc 可以使用 noise protocol。
 - [x] 在设计 frame 的时候，如果我们的压缩方法不止 gzip 一种，而是服务器或客户端都会根据各自的情况，在需要的时候做某种算法的压缩。假设服务器和客户端都支持 gzip、lz4 和 zstd 这三种压缩算法。那么 frame 该如何设计呢？需要用几个 bit 来存放压缩算法的信息？
 - [x] 目前我们的 client 只适合测试，你可以将其修改成一个完整的命令行程序么？小提示，可以使用 clap 或 structopt，用户可以输入不同的命令；或者做一个交互式的命令行，使用 shellfish 或 rustyline，就像 redis-cli 那样。
--- src/service/topic.rs
+++ src/service/topic.rs
@@ -1,5 +1,6 @@
 use crate::{CommandResponse, KvError, Value};
 use dashmap::{DashMap, DashSet};
+use glob::Pattern;
 use std::sync::{
     atomic::{AtomicU32, Ordering},
     Arc,
@@ -27,6 +28,12 @@ pub trait Topic: Send + Sync + 'static {
 
     /// 往主题里面发布某内容
     fn publish(self, name: String, value: Arc<CommandResponse>);
+
+    /// 订阅某个模式
+    fn psubscribe(self, pattern: &'static str) -> mpsc::Receiver<Arc<CommandResponse>>;
+
+    /// 退订某个模式
+    fn punsubscribe(self, pattern: &'static str, id: u32) -> Result<u32, KvError>;
 }
 
 /// 用于主题发布和订阅的数据结构
@@ -36,6 +43,8 @@ pub struct Broadcaster {
     topics: DashMap<String, DashSet<u32>>,
     /// 所有的订阅列表
     subscriptions: DashMap<u32, mpsc::Sender<Arc<CommandResponse>>>,
+    /// 所有的模式订阅列表
+    patterns: DashMap<Pattern, DashSet<u32>>,
 }
 
 impl Broadcaster {
@@ -47,6 +56,20 @@ impl Broadcaster {
                 info!("Topic: {:?} is deleted", &name);
                 drop(v);
                 self.topics.remove(&name);
+            }
+        }
+        debug!("Subscription {} is removed!", id);
+        self.subscriptions.remove(&id).map(|(id, _)| id)
+    }
+
+    fn remove_pattern(&self, pattern: Pattern, id: u32) -> Option<u32> {
+        if let Some(v) = self.patterns.get_mut(&pattern) {
+            v.remove(&id);
+
+            if v.is_empty() {
+                info!("Topic: {:?} is deleted", &pattern);
+                drop(v);
+                self.patterns.remove(&pattern);
             }
         }
         debug!("Subscription {} is removed!", id);
@@ -85,11 +108,51 @@ impl Topic for Arc<Broadcaster> {
         rx
     }
 
+    #[instrument(name = "pattern_subscribe", skip_all)]
+    fn psubscribe(self, pattern: &'static str) -> mpsc::Receiver<Arc<CommandResponse>> {
+        let pattern = Pattern::new(pattern).unwrap();
+        let id = {
+            // topics 表中看看有没有 name 对应的 entry，有则获取，没有则创建
+            let entry = self.patterns.entry(pattern).or_default();
+            let id = get_next_subscription_id();
+            entry.value().insert(id);
+            id
+        };
+
+        // 生成一个 mpsc 的 channel
+        let (tx, rx) = mpsc::channel(BROCASTER_CAPACITY);
+        let v: Value = (id as i64).into();
+
+        let tx1 = tx.clone();
+        // 当你 subscribe 一个 topic 的时候，可以先从其中 receive 相关的 subcribe id
+        tokio::spawn(async move {
+            if let Err(e) = tx1.send(Arc::new(v.into())).await {
+                // TODO: 这个很小概率发生，但目前我们没有善后
+                warn!("Failed to send subscription id: {}. Error: {:?}", id, e);
+            }
+        });
+
+        // 把 tx 存储到 subscription table 中
+        self.subscriptions.insert(id, tx);
+        debug!("Subscription {} is added", id);
+
+        rx
+    }
+
     #[instrument(name = "topic_unsubscribe", skip_all)]
     fn unsubscribe(self, name: String, id: u32) -> Result<u32, KvError> {
         match self.remove_subscription(name, id) {
             Some(v) => Ok(v),
             None => Err(KvError::NotFound(format!("subscription {}", id))),
+        }
+    }
+
+    #[instrument(name = "pattern_unsubscribe", skip_all)]
+    fn punsubscribe(self, pattern: &'static str, id: u32) -> Result<u32, KvError> {
+        let pattern = Pattern::new(pattern).unwrap();
+        match self.remove_pattern(pattern, id) {
+            Some(v) => Ok(v),
+            None => Err(KvError::NotFound(format!("pattern {}", id))),
         }
     }
 
@@ -120,6 +183,33 @@ impl Topic for Arc<Broadcaster> {
                 for id in ids {
                     self.remove_subscription(name.clone(), id);
                 }
+            }
+
+            let mut pattern_ids = vec![];
+
+            let patter_matched = self
+                .patterns
+                .iter()
+                .filter_map(|pair| match pair.key().matches(&name[..]) {
+                    true => Some(pair),
+                    false => None,
+                })
+                .collect::<Vec<_>>();
+
+            for element in patter_matched {
+                for id in element.value().clone().into_iter() {
+                    if let Some(tx) = self.subscriptions.get(&id) {
+                        if let Err(e) = tx.send(value.clone()).await {
+                            warn!("Publish to {} failed! error: {:?}", id, e);
+                            // client 中断连接
+                            pattern_ids.push((element.key().clone(), id));
+                        }
+                    }
+                }
+            }
+
+            for (pattern, id) in pattern_ids {
+                self.remove_pattern(pattern, id);
             }
         });
     }
@@ -161,5 +251,56 @@ mod tests {
         assert!(result.is_none());
         let res2 = stream2.recv().await.unwrap();
         assert_res_ok(&res2, &[v.clone()], &[]);
+    }
+
+    #[tokio::test]
+    async fn pattern_pub_sub_should_work() {
+        let b = Arc::new(Broadcaster::default());
+        let topic_1 = "chat.rust".to_string();
+        let topic_2 = "chat.exilir".to_string();
+        let topic_3 = "chat".to_string();
+
+        let mut stream1 = b.clone().subscribe(topic_1);
+        let mut stream2 = b.clone().subscribe(topic_2);
+        let mut stream3 = b.clone().subscribe(topic_3);
+        let mut stream4 = b.clone().psubscribe("chat.*");
+        let id1: i64 = stream1.recv().await.unwrap().as_ref().try_into().unwrap();
+        let id2: i64 = stream2.recv().await.unwrap().as_ref().try_into().unwrap();
+        let id3: i64 = stream3.recv().await.unwrap().as_ref().try_into().unwrap();
+        let id4: i64 = stream4.recv().await.unwrap().as_ref().try_into().unwrap();
+
+        assert_ne!(id1, id2);
+        assert_ne!(id2, id3);
+        assert_ne!(id3, id4);
+        let v1: Value = "chat.rust".into();
+        let v2: Value = "chat.exilir".into();
+        let v3: Value = "chat".into();
+
+        b.clone()
+            .publish("chat.rust".to_string(), Arc::new(v1.clone().into()));
+        b.clone()
+            .publish("chat.exilir".to_string(), Arc::new(v2.clone().into()));
+        b.clone()
+            .publish("chat".to_string(), Arc::new(v3.clone().into()));
+
+        let result1 = stream1.recv().await.unwrap();
+        let result2 = stream2.recv().await.unwrap();
+        let result3 = stream3.recv().await.unwrap();
+        let result4 = stream4.recv().await.unwrap();
+        assert_res_ok(&result1, &[v1.clone()], &[]);
+        assert_res_ok(&result2, &[v2.clone()], &[]);
+        assert_res_ok(&result3, &[v3.clone()], &[]);
+        assert_res_ok(&result4, &[v1.clone()], &[]);
+        let result4 = stream4.recv().await.unwrap();
+        assert_res_ok(&result4, &[v2.clone()], &[]);
+
+        b.clone().punsubscribe("chat.*", id4 as _).unwrap();
+        b.clone()
+            .publish("chat.exilir".to_string(), Arc::new(v2.clone().into()));
+
+        let result = stream4.recv().await;
+        assert!(result.is_none());
+        let res2 = stream2.recv().await.unwrap();
+        assert_res_ok(&res2, &[v2.clone()], &[]);
     }
 }
